8cd33a633042515cad9ccadcd493dad606afd121
Iraklis Mathiopoulos <iam1980@users.noreply.github.com>
2017-03-22 18:52:52 +0000

    added screen package

--- msf_root.sh
+++ msf_root.sh
@@ -9,7 +9,7 @@ cd ~
 
 apt-get-update
 
-apt-get -y install autoconf bison build-essential curl git-core libapr1 libaprutil1 libcurl4-openssl-dev libgmp3-dev libpcap-dev libpq-dev libreadline6-dev libsqlite3-dev libssl-dev libsvn1 libtool libxml2 libxml2-dev libxslt-dev libyaml-dev locate ncurses-dev openssl wget xsel zlib1g zlib1g-dev ruby-dev
+apt-get -y install screen autoconf bison build-essential curl git-core libapr1 libaprutil1 libcurl4-openssl-dev libgmp3-dev libpcap-dev libpq-dev libreadline6-dev libsqlite3-dev libssl-dev libsvn1 libtool libxml2 libxml2-dev libxslt-dev libyaml-dev locate ncurses-dev openssl wget xsel zlib1g zlib1g-dev ruby-dev
 
 git clone https://github.com/rapid7/metasploit-framework.git
 
