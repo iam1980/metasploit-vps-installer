93b5c0175e01a772f65c3f23eb340686fe5ec22b
Iraklis Mathiopoulos <iam1980@users.noreply.github.com>
2017-03-22 16:45:22 +0000

    rvm install ruby-2.3.3 fix

--- msf_vps_installer.sh
+++ msf_vps_installer.sh
@@ -30,7 +30,7 @@ curl -L https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 cd ~/metasploit-framework
 echo -e "${PURPLE}Now we wait...${NC}"
-rvm --install .ruby-version
+rvm install ruby-2.3.3
 gem install bundler
 bundle install
 
